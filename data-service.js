85274bc34758b7598966ccdef6a51434fcc0689c
Rudra Bhosale <rbhosale1@myseneca.ca>
2022-11-18 16:24:28 -0500

    Done

---
+++ data-service.js
@@ -0,0 +1,124 @@
+
+var employee_data = [];
+var department_data = [];
+
+exports.initialize = function(){
+    const fs = require('node:fs');
+
+    fs.readFile('./data/employees.json',(err,data)=>{
+        if (err) reject("Failure to read file employees.json!");
+        employee_data = JSON.parse(data);
+    });
+
+    fs.readFile('./data/departments.json',(err,data)=>{
+        if (err) reject("Failure to read file departments.json!");
+        department_data = JSON.parse(data);
+    });
+
+    return new Promise(function(resolve, reject){
+        console.log("initialize called");
+        resolve("Data succesfully initialized!");
+    });
+}
+
+exports.getAllEmployees = function(){
+    return new Promise(function(resolve, reject){
+            resolve(employee_data);
+            reject(reason);
+    });
+}
+
+exports.getManagers = function(){
+    return new Promise(function(resolve, reject){  
+        const manager_array = [];
+        for(employee of employee_data){
+            if(employee.isManager == true){
+                manager_array.push(employee);
+            }
+        }
+        resolve(manager_array);
+        reject(reason);
+    });
+}
+
+exports.getDepartments = function(){
+    return new Promise(function(resolve, reject){ 
+            resolve(department_data);
+            reject(reason);
+    });
+}
+
+exports.addEmployee = function(employeeData){
+    return new Promise(function(resolve, reject){
+        if(employeeData.isManager == null){
+            employeeData.isManager = false;
+        }
+        else{
+            employeeData.isManager = true; 
+        }
+
+        employeeData.employeeNum = employee_data.length + 1;
+        employee_data.push(employeeData);
+        resolve();
+    });
+}
+
+exports.getEmployeesByStatus = function(status){
+    return new Promise(function(resolve, reject){
+        const temp = [];
+        for(employee of employee_data){
+            if(employee.status == status){
+                temp.push(employee);
+            }
+        }
+        resolve(temp);
+    });
+}
+
+exports.getEmployeesByDepartment = function(Department){
+    return new Promise(function(resolve, reject){
+        const temp = [];
+        for(employee of employee_data){
+            if(employee.department == Department){
+                temp.push(employee);
+            }
+        }
+        resolve(temp);
+    });
+}
+
+exports.getEmployeesByManager = function(Manager){
+    return new Promise(function(resolve, reject){
+        const temp = [];
+        for(employee of employee_data){
+            if(employee.employeeManagerNum == Manager){
+                temp.push(employee);
+            }
+        }
+        resolve(temp);
+    });
+}
+
+exports.getEmployeeByNum = function(num){
+    return new Promise(function(resolve, reject){
+        var temp;
+        for(employee of employee_data){
+            if(employee.employeeNum == num){
+                temp = employee;
+            }
+        }
+        resolve(temp);
+    });
+}
+
+exports.updateEmployee = function(employeeData){
+    return new Promise(function(resolve, reject){
+        for(employee of employee_data){
+            if(employee.employeeNum == employeeData.employeeNum){
+                var index = employee_data.indexOf(employee);
+                employee_data[index] = employeeData;
+            }
+        }
+        resolve();
+    });
+}
---
+++ server.js
@@ -0,0 +1,174 @@
+/*************************************************************************
+* BTI325– Assignment 3
+* I declare that this assignment is my own work in accordance with Seneca Academic
+Policy. And I have taken help from my friend inorder to complete this and ihope this wont attract any
+integrity problem for me and him
+*
+*
+* Name: Rudra Bhosale Student ID: 162207211 Date: 1/11/2022
+*
+* Your app’s URL (from heroku) : https://calm-cove-20683.herokuapp.com/
+*
+*************************************************************************/ 
+const data_services = require("./data-service.js")
+var express = require("express"); 
+var app = express();
+var path = require("path"); 
+const fs =require('fs');
+var exphbs = require('express-handlebars');
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+var multer =  require('multer');
+var storage = multer.diskStorage({
+
+    destination : "./public/images/uploaded", 
+    filename: function(req, file, cb){
+        cb(null, Date.now() + path.extname(file.originalname)); 
+    }
+  
+});
+
+const upload = multer({storage : storage});
+app.use(express.static('public'));
+
+app.engine('.hbs',exphbs.engine({
+    extname:'.hbs', 
+    defaultLayout:'main',
+    helpers:{
+        navLink:function(url, options){
+            return '<li' + ((url==app.locals.activeRoute)? ' class="active"':'')
+                +'><a href="'+url+'">'+options.fn(this)+'</a></li>'
+        },
+        equal:function(lvalue, rvalue, options){
+            if(arguments.length<3)
+                throw new Error("Handlerbars Helper equal needs 2 parameters");
+            if(lvalue != rvalue){
+                return options.inverse(this);
+            }else{
+                return options.fn(this);
+            }
+        }
+    }
+}));
+app.set('view engine','.hbs');
+
+var HTTP_PORT = process.env.PORT || 8080; 
+function onHttpStart(){
+    console.log("Express http server listening on: " + HTTP_PORT);
+}
+
+app.set('view engine','.hbs');
+app.use(function(req,res,next){
+    let route=req.baseUrl + req.path;
+    app.locals.activeRoute = (route=="/")? "/":route.replace(/\/$/,"");
+    next();
+});
+
+
+app.get("/", (req, res) => {
+    res.render("home");
+});
+
+app.get("/about", (req, res) => {
+    res.render("about");
+});
+
+app.get("/employees/add", (req, res) => {
+    
+    res.render("addEmployee");
+});
+
+app.get("/images/add", (req, res) => {
+    res.render("addImage");
+});
+
+app.get('/employee/:employeeNum', (req, res) => {
+    data_services.getEmployeeByNum(req.params.employeeNum)
+    .then((data) => res.render("employee",{employee:data}))
+    .catch(()=>{res.render("employee",{message:"no results"})
+})
+});
+
+//modified routes
+app.get('/employees', (req, res) => {
+    if(req.query.status) {
+        data_services.getEmployeesByStatus(req.query.status)
+            .then((data) => res.render("employees",{employees:data}))
+            .catch(() => res.render("employees",{message: "no results"}))
+    }else if(req.query.manager){
+        data_services.getEmployeesByManager(req.query.manager)
+            .then((data) => res.render("employees",{employees:data}))
+            .catch(() => res.render("employees",{message: "no results"}))
+    }else if(req.query.department){
+        data_services.getEmployeesByDepartment(req.query.department)
+            .then((data) => res.render("employees",{employees:data}))
+            .catch(() => res.render("employees",{message: "no results"}))
+    }else{
+        data_services.getAllEmployees()
+            .then((data) => res.render("employees",{employees:data}))
+            .catch(() => res.render("employees",{message: "no results"}))
+    }
+});
+
+/*app.get("/managers",function(req,res){
+    data_services.getManagers().then(function(data){
+        res.json(data);
+    })
+    .catch(function(err){
+        res.json({"message":err})
+   // })
+})*/
+
+app.get('/departments', (req, res) => {
+    data_services.getDepartments()
+        .then((data) => res.render("departments",{departments:data}))
+        .catch(() => res.render("departments",{"message": "no results"}))
+})
+
+ 
+app.get("/images/add", function(req,res){
+    res.sendFile(path.join(__dirname + views + "addImage.html"));
+  })
+
+app.get("/images", (req, res) => {
+    fs.readdir("./public/images/uploaded", function(err, imageFile){
+        res.render("images",  { data: imageFile, title: "Images" });
+    })
+
+})
+
+app.post("/images/add", upload.single("imageFile"), function(req, res){  
+    res.redirect("/images")
+  });
+
+
+  app.post('/employees/add', function(req, res) {
+    data_services.addEmployee(req.body)
+        .then(res.redirect('/employees'))
+        .catch((err) => res.json({"message": err}))   
+}) 
+
+// A filter in which employye number is taken and detailes are shown
+
+
+app.post("/employee/update", function(req, res){
+    data_services.updateEmployee(req.body)
+    .then(res.redirect('/employees'))
+});
+ 
+app.use((req, res) => {
+    res.status(404).send("Sorry Guys This page is not found");
+  });
+
+
+data_services.initialize().then(function(data){
+    app.listen(HTTP_PORT, onHttpStart);
+  
+  }).catch(function(err){
+    console.log(err);
+  })
+
+
+  
+
+  
